0f015e19b20f3453a78baece217c3294ffcd4814
NadiaEs829 <nadia_amores@hotmail.com>
2024-12-09 18:39:21 +0000

    end of 12//9

--- SLLTester.java
+++ SLLTester.java
@@ -1,5 +1,26 @@
+//Nadia Escamilla, 12/9/24, pd: 7, CP3
+//The purpose of this class is to practice with Stacks and Queues by addding and modifying these lists
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
 class SLLTester {
   public static void main(String[] args) {
+    //Part 1 - practice with Stacks
+    Scanner input = new Scanner(System.in);
+    Stack<String> myStk = new Stack<String>();
 
+    for(int i = 0; i < 5; i++){
+      System.out.println("Enter a whole number");
+        String num = input.nextLine();
+        myStk.push(num);
+    }//end for loop
+    Stack<String> tempStk = myStk;
+    while(!myStk.isEmpty()){
+      if(myStk.peek().equals())
+    }
+
+//Part 2 - practice with queues
   } // end main
 } // end class
